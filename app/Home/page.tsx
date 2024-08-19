7f67b0b0766c5abf3df4741afccb50ba06fe5853
dinith vanderlan <dinithvanderlan@gmail.com>
2024-08-19 14:28:50 +0530

    features updated

---
+++ app/Home/page.tsx
@@ -0,0 +1,24 @@
+import { Features } from "./_sections/features";
+import Head from "next/head";
+
+export default function Page() {
+  return (
+    <div>
+      <Head>
+        <title>Next js scroll Animations with Framer Motion</title>
+        <meta
+          name="description"
+          content="Scroll animations with Framer Motion"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+      <div className="relative z-10 w-full overflow-x-clip">
+         
+          <Features />
+          
+        </div>
+      </main>
+    </div>
+  );
+}
