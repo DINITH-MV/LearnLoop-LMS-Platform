4dd6c5445913dfe9964e767a0dd51d37be6abd2f
dinith vanderlan <dinithvanderlan@gmail.com>
2024-08-23 12:23:08 +0530

    Update more-features.tsx

--- app/Home/_sections/more-features.tsx
+++ app/Home/_sections/more-features.tsx
@@ -63,13 +63,13 @@ export const MoreFeatures = () => {
         style={{ opacity, x }} // Apply x transform instead of y
         className="flex flex-col items-center justify-star "
       >
-        <div className="w-[540px] text-center py-[28px] bg-[linear-gradient(135deg,#fba946,#ffba7f)] rounded-r-[70px] p-[20px] ml-[0px]">
+        <div className="mt-[5px] w-[540px] text-center py-[28px] bg-[linear-gradient(135deg,#fba946,#ffba7f)] rounded-r-[70px] p-[20px] ml-[0px]">
           <p className="text-[32pt] text-left ml-[20px] text-[#80643ae5] ptSans font-semibold leading-[1.4]">
             DISCOVER OUR LATEST<br/> AI FEATURES
           </p>
         </div>
 
-        <div className="mt-[30px] rounded-tl-[50px] rounded-br-[50px] grid w-[1100px] bg-gradient-to-r from-black via-gray-800 to-gray-900 ... border-black border px-[10px] max-w-[120rem] grid-cols-3 gap-[5px] py-[10px]">
+        <div className="mt-[37px] rounded-tl-[50px] rounded-br-[50px] grid w-[1100px] bg-gradient-to-r from-black via-gray-800 to-gray-900 ... border-black border px-[10px] max-w-[120rem] grid-cols-3 gap-[5px] py-[10px]">
           {content.map(({ icon: Icon, title, text }) => (
             <div key={title} className="bg-[#fff] rounded-tl-[40px] rounded-br-[40px] h-[260px] w-[340px] p-[10px] m-[10px]">
               <span className="mb-4 flex h-32 w-32 items-center justify-center rounded-tl-[30px] bg-gradient-to-r from-black via-gray-800 to-gray-900 ...">
