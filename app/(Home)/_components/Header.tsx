b46adfffa47cb0fcdd5c546a89b6d1ecb01dd5aa
dinith vanderlan <dinithvanderlan@gmail.com>
2024-09-23 23:52:50 +0530

    Update Header.tsx

--- app/(Home)/_components/Header.tsx
+++ app/(Home)/_components/Header.tsx
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <div className="w-full pt-[30px] z-[199] relative">
-      <div className="text-center text-[25pt] m-auto w-[600px] flex justify-around">
+      <div className="text-center text-[15.5pt] m-auto w-[600px] flex justify-around">
         <span>HOME</span>
         <span onClick={() => window.scrollTo({ top: 5245, behavior: 'smooth' })}>FEATURES</span>
         <span>COURSES</span>
