b9649099885a42e0fb361f604d633c02c6596208
dinith vanderlan <dinithvanderlan@gmail.com>
2024-09-26 14:33:14 +0530

    AI assistant updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { MoreFeatures } from "./(Home)/_sections/more-features";
 import { useState, useEffect } from "react";
 import Header from "./(Home)/_components/Header";
 import Footer from "./(Home)/_components/Footer";
+import AIAssistant from "./(Home)/_sections/AI-Assistant";
 
 export default function Page() {
   const [isClient, setIsClient] = useState(false);
@@ -35,41 +36,7 @@ export default function Page() {
         <div className="z-[100] relative">
           <Footer />
         </div>
-        <div className="z-[100] fixed bottom-20 right-[105px] h-[70px] w-[70px] rounded-[30px]">
-          <span
-            className="fa-stack fa-2x fa-shake"
-            style={
-              {
-                fontSize: "24pt",
-                top: "3px",
-                left: "-4.5px",
-              } as React.CSSProperties
-            }
-          >
-            <i
-              className="fa-solid fa-square fa-stack-2x"
-              style={
-                {
-                  color: "#fb5d5d",
-                  borderRadius: "30px",
-                } as React.CSSProperties
-              }
-            ></i>
-            <i
-              className="fa-duotone fa-solid fa-message-bot z-[102]"
-              style={
-                {
-                  "--fa-primary-color": "#fff",
-                  "--fa-secondary-color": "#fff",
-                  "--fa-secondary-opacity": "0.8",
-                  fontSize: "24pt",
-                  top: "0px",
-                  left: "20px",
-                } as React.CSSProperties
-              }
-            ></i>
-          </span>
-        </div>
+        <AIAssistant/>
       </main>
     </div>
   );
